44144c101a54f6d14d2464ef23b336ed80746188
issxjl2015 <issxjl2015@outlook.com>
2019-11-09 00:13:55 +0800

    新增二叉树非递归前序遍历的实现

--- java/chapter3_tree/TreeNodeTest1.java
+++ java/chapter3_tree/TreeNodeTest1.java
@@ -2,6 +2,7 @@ package chapter3_tree;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class TreeNodeTest1 {
 
@@ -72,6 +73,27 @@ public class TreeNodeTest1 {
         System.out.println(node.data);
     }
 
+    /**
+     * 二叉树非递归——前序遍历
+     * */
+    public static void preOrderTraveralWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            // 迭代访问节点的左孩子，并入栈
+            while (treeNode != null) {
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild;
+            }
+            // 如果节点没有左孩子，则弹出栈顶元素，访问节点右孩子
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.rightChild;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList<Integer> inputList = new LinkedList<>(Arrays.asList(new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4}));
         TreeNode treeNode = createBinaryTree(inputList);
@@ -83,5 +105,8 @@ public class TreeNodeTest1 {
         System.out.println("=======================");
         System.out.println("后序遍历：");
         postOrderTraveral(treeNode);
+        System.out.println("=======================");
+        System.out.println("非递归方式先序遍历：");
+        preOrderTraveralWithStack(treeNode);
     }
 }
